dbde16f19722864155bd88a493efa89971887ed7
DooYoung Choi <dy.choi@okestro.com>
2021-06-04 17:56:42 +0900

    Initial commit

---
+++ azure-api/src/main/java/api/BootApplication.java
@@ -0,0 +1,16 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author dy20c
+ * @Create 6/4/21 10:39 AM
+ * @Package api
+ */
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
---
+++ azure-api/src/main/java/api/azure/OAuth.java
@@ -0,0 +1,9 @@
+package api.azure;
+
+/**
+ * @Author dy20c
+ * @Create 6/4/21 10:43 AM , Sout Korea
+ * @Package api.azure
+ */
+public class OAuth {
+}
